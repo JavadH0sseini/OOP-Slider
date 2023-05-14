44431d82f53e808a5196c6298421bfa6e15ccb8b
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-05-15 02:39:47 +0330

    a minor issues removed from index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import "./styles/main.scss";
 import ImageSlider from "./ImageSlider";
 import data from "./data.json";
 import Button from "./Button";
-const container = document.querySelector(".container");
-const sliderContainer = document.querySelector(".slider");
-const sliderCntls = document.querySelector(".slider__cntrls");
 
+addEventListener("DOMContentLoaded", (event) => {
+  const container = document.querySelector(".container");
+  const sliderContainer = document.querySelector(".slider");
+  const sliderCntls = document.querySelector(".slider__cntrls");
 
-const slider = new ImageSlider(sliderContainer, data, 5000);
-slider.render();
+  const slider = new ImageSlider(sliderContainer, data, 5000);
+  slider.render();
 
-
-const prev = new Button(sliderCntls, "قبلی", slider.showPrevious, slider, 'prev__btn');
-prev.render();
-const next = new Button(sliderCntls, "بعدی", slider.showNext, slider, 'next__btn');
-next.render();
-
-
-
+  const prev = new Button(
+    sliderCntls,
+    "قبلی",
+    slider.showPrevious,
+    slider,
+    "prev__btn"
+  );
+  prev.render();
+  const next = new Button(
+    sliderCntls,
+    "بعدی",
+    slider.showNext,
+    slider,
+    "next__btn"
+  );
+  next.render();
+});
